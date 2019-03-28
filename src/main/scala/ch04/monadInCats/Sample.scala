0320009a914ca894fdad79d22c53c7cec1c5c3d8
jang <dragoalie@gmail.com>
2019-03-29 01:04:11 +0900

    Monad in cats (wip)

---
+++ src/main/scala/ch04/monadInCats/Sample.scala
@@ -0,0 +1,23 @@
+package ch04.monadInCats
+
+import cats.Monad
+import cats.instances.vector._
+import scala.concurrent.Future // for Monad
+
+import cats.instances.future._ // for Monad
+import scala.concurrent._
+import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContext.Implicits.global // Bringing the ExecutionContext into scope
+
+object Sample {
+
+  def main(args: Array[String]): Unit = {
+    val res = Monad[Vector].flatMap(Vector(1, 2, 3))(a => Vector(a, a * 10))
+    println(res) // Vector(1, 10, 2, 20, 3, 30)
+
+    val fm = Monad[Future]
+
+  }
+
+}
