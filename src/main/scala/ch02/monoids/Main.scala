c413ccd95b6261b7b7cf45274f74255ae1ca5e11
jang <dragoalie@gmail.com>
2019-04-01 22:27:54 +0900

    Execute in main with apply

---
+++ src/main/scala/ch02/monoids/Main.scala
@@ -0,0 +1,23 @@
+package ch02.monoids
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+    val intSetMonoid = Monoid[Set[Int]]
+    val res = intSetMonoid.combine(Set(1, 2), Set(2, 3))
+
+    val strSetMonoid = Monoid[Set[String]]
+    val res01 = strSetMonoid.combine(Set("A", "B"), Set("B", "C"))
+
+    println(res)
+    println(res01)
+
+    val intSetSemigroup = Semigroup[Set[Int]]
+    val res02 = intSetSemigroup.combine(Set(1, 2), Set(2, 3))
+
+    println(res02)
+
+  }
+
+}
