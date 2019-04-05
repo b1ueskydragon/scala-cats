99022345786f95620fa5ea2c0685d1328719792f
jang <dragoalie@gmail.com>
2019-04-06 00:10:25 +0900

    combine binary params in cats

--- src/main/scala/ch02/cats/Example.scala
+++ src/main/scala/ch02/cats/Example.scala
@@ -3,6 +3,11 @@ package ch02.cats
 import cats.Monoid
 import cats.instances.string._ // for Monoid
 
+import cats.instances.int._ // for Monoid; to assemble a Monoid[Option[Int]]
+import cats.instances.option._ // for Monoid; to assemble a Monoid[Option[Int]]
+
+import cats.syntax.semigroup._ // for combine operation
+
 object Example {
 
   def main(args: Array[String]): Unit = {
@@ -12,6 +17,19 @@ object Example {
 
     println(res)
     println(res01)
+
+    val x = Option(11)
+    val y = Option(99)
+
+    // Option(x) + Option(y) = Option(x + y)
+    // Int + Int = Int; closed
+    val res02 = Monoid[Option[Int]].combine(x, y)
+
+    println(res02) // Some(110) == Option(110)
+
+    val res03 = "A" |+| "B" |+| Monoid[String].empty
+
+    println(res03)
 
   }
 
