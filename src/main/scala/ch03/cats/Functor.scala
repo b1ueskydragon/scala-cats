6915de0c923ed300b49925224adb9ebf80bfd4f5
jang <dragoalie@gmail.com>
2019-04-19 00:44:41 +0900

    Funtor laws; Type constructor

---
+++ src/main/scala/ch03/cats/Functor.scala
@@ -0,0 +1,18 @@
+package ch03.cats
+
+import scala.language.higherKinds // enable
+
+/** Identity:
+  * calling map with the identity function == doing nothing
+  * | fa.map(a => a) == fa
+  *
+  * Composition:
+  * mapping with two functions f and g == mapping with f and then mapping with g
+  * | fa.map(g(f(_))) == fa.map(f).map(g)
+  *
+  * @tparam F type constructor
+  *           (take a single parameter and produce type)
+  */
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
